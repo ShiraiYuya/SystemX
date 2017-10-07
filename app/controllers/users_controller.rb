e66c503429574d5c046b09d034ce4557aec6b8e0
vagrant <vagrant@localhost.localdomain>
2017-10-07 10:05:05 +0000

    makegraph

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,12 @@
 class UsersController < ApplicationController
   def index
+	@chart_data = [
+	{name:"F",data:[["Mon",10],["Tue",20],["Wed",10],["Thu",20],["Fri",10],["Sat",20]]},
+	{name:"全卵",data:[["Mon",10],["Tue",20],["Wed",10],["Thu",40],["Fri",10],["Sat",20]]},
+	{name:"その他",data:[["Mon",10],["Tue",20],["Wed",10],["Thu",20],["Fri",10],["Sat",20]]},
+	{name:"F作り置き",data:[["Mon",10],["Tue",20],["Wed",10],["Thu",20],["Fri",10],["Sat",20]]},
+	{name:"全卵作り置き",data:[["Mon",10],["Tue",20],["Wed",10],["Thu",20],["Fri",10],["Sat",20]]}
+	]
   end
 
   def conf
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   get 'users/stock'
   
   post 'index' => 'users/conf'
+  
+  post 'users/conf' => 'users/conf'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
