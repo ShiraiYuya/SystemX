49094efb7edcac5fe4ddda84a18594143eaf7951
vagrant <vagrant@localhost.localdomain>
2018-01-31 06:54:43 +0000

    seed

--- app/assets/python/config.py
+++ app/assets/python/config.py
@@ -1,17 +1,18 @@
 # -*- coding: utf-8 -*-
 
-'''
+
 #MySQL (local)
 
 user = 'root'
 host = 'localhost'
 passwd = 'AwajiSys@15'
 db = 'SystemX_development'
-'''
 
+'''
 #MySQL (heroku)
 
 user = 'b5145f4c532cd9'
 host = 'us-cdbr-iron-east-05.cleardb.net'
 passwd = '27499f4c'
 db = 'heroku_761e024be925ffb'
+'''
